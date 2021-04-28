da57a177ab7cc9ec97e27f20a500837e3b30ffb8
James Lau <1402829+jameslau@users.noreply.github.com>
2021-04-28 15:04:47 -0400

    adding css module components

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,27 @@
 import React from 'react'
-import { Link } from "gatsby"
+import { Link } from 'gatsby'
+
+// import './header.module.scss'
+
+// define object
+import * as headerStyles from './header.module.scss'
+
 
 const Header = () => {
   return (
-    <header>
-      <h1>JAMES LAU</h1>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link className={headerStyles.title} to="/">
+          JAMES LAU
+        </Link>
+      </h1>
       <nav>
-        <ul>
-          <li><Link to="/">home</Link></li>
-          <li><Link to="/about">about</Link></li>
-          <li><Link to="/portfolio">portfolio</Link></li>
-          <li><Link to="/blog">blog</Link></li>
-          <li><Link to="/contact">contact</Link></li>
+        <ul className={headerStyles.navList}>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/portfolio">Portfolio</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
