1d8a9928854faec1e0567bda12dd916c15d27b70
jameslau <1402829+jameslau@users.noreply.github.com>
2021-07-29 23:38:14 -0400

    adding date to blog post

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -15,6 +15,7 @@ export default class PostTemplate extends React.Component {
         <div>
           <div>
             <h1>{post.title}</h1>
+            <p>{post.date}</p>
             <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
             <div className="post-meta">
             </div>
@@ -31,6 +32,7 @@ export const pageQuery = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
+        date
         title
       }
       fields {
@@ -39,42 +41,3 @@ export const pageQuery = graphql`
     }
   }
 `;
-
-
-// import React from 'react'
-// import { graphql } from 'gatsby'
-
-// import Layout from '../components/layout'
-
-// export default function PostTemplate({ data, pageContext }) {
-//   const { slug } = pageContext;
-//   const postNode = data.markdownRemark;
-//   const post = postNode.frontmatter;
-//   if (!post.id) {
-//     post.id = slug;
-//   }
-
-//   return (
-//     <Layout>
-//       <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-//       <p>{props.data.markdownRemark.frontmatter.date}</p>
-
-//       <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
-//     </Layout>
-//   );
-// }
-
-// /* eslint no-undef: "off" */
-// export const pageQuery = graphql`
-//   query BlogPostBySlug($slug: String!) {
-//     markdownRemark(fields: { slug: { eq: $slug } }) {
-//       html
-//       frontmatter {
-//         title
-//       }
-//       fields {
-//         slug
-//       }
-//     }
-//   }
-// `;
